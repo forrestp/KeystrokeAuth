69c8dba830422b3c071e3a2c75845f8613401112
Kenny <ksiebert@mit.edu>
2013-12-09 12:51:22 -0500

    mahabolnis distance with identity matrix, will add computation of S matrix and figure out what acceptable thresholds are next

--- rhythmCheck.py
+++ rhythmCheck.py
@@ -1,2 +1,22 @@
+import numpy as np
+
 def checkTimings(testTiming, realTiming):
-    return True
+	np_test = np.array(testTiming)
+	np_real = np.array(realTiming)
+	n = len(testTiming)
+
+	# Currenly the identity matrix
+	S = np.identity(n)
+
+	# calculate mahabolonis distance
+	temp = np.dot(np.dot(np.transpose(np_test - np_real), np.linalg.inv(S)),(np_test - np_real))
+	print temp
+	mh_distance = temp ** 0.5
+
+	print mh_distance
+	return True
+
+
+array1 = [1,10,15,20]
+array2 = [2,12,18,22]
+checkTimings(array1,array2)
